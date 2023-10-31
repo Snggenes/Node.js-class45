6e06b14456ff9145d7d2b46aa93767436d9a9775
Enes <snggenes@gmail.com>
2023-10-31 21:03:57 +0100

    node1-2

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -1,14 +1,42 @@
-/**
- * Exercise 3: Create an HTTP web server
- */
+const http = require("http");
+const fs = require("fs");
 
-const http = require('http');
-
-//create a server
 let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+  const reqUrl = req.url;
+  switch (reqUrl) {
+    case "/":
+      fs.readFile("index.html", (err, data) => {
+        if (err) {
+          throw err;
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+          res.end();
+        }
+      });
+      break;
+    case "/index.js":
+      fs.readFile("index.js", (err, data) => {
+        if (err) {
+          throw err;
+        } else {
+          res.writeHead(200, { "Content-Type": "application/javascript" });
+          res.write(data);
+          res.end();
+        }
+      });
+      break;
+    case "/index.css":
+      fs.readFile("index.css", (err, data) => {
+        if (err) {
+          throw err;
+        } else {
+          res.writeHead(200, { "Content-Type": "text/css" });
+          res.write(data);
+          res.end();
+        }
+      });
+  }
 });
 
-server.listen(3000); // The server starts to listen on port 3000
+server.listen(3000);
